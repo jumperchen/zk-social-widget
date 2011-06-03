ea7f43a3be53de6472cc17b266477325dc4e6d9e
simonpai <simonpai@2e3d9f68-5b0b-a1c6-e813-6aaad624f7ac>
2011-06-03 03:18:54 +0000

    add fb:comments skeleton, make XFBML default mold

---
+++ src/main/java/org/zkoss/social/facebook/Comments.java
@@ -0,0 +1,29 @@
+/* Comment.java
+
+{{IS_NOTE
+ Purpose:
+  
+ Description:
+  
+ History:
+  Jun 3, 2011 11:03:07 AM , Created by simonpai
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.social.facebook;
+
+/**
+ *
+ * @author simonpai
+ */
+public class Comments extends FacebookElement {
+	
+	private static final long serialVersionUID = -616899817943777534L;
+	
+	
+	
+}
---
+++ src/main/resources/web/js/soc/fb/mold/like-if.js
@@ -0,0 +1,9 @@
+/*
+ * 
+ */
+function (out) {
+	out.push('<iframe src="http://www.facebook.com/plugins/like.php?');
+	this._wgtAttrs(out, true);
+	out.push('" scrolling="no" frameborder="0" allowTransparency="true"', 
+			' style="border:none; overflow:hidden; width:450px; height:80px;"></iframe>'); // TODO: height & width
+}
