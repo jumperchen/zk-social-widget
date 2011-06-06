3af370947dee48a18ca0e365e51abea952985038
simonpai <simonpai@2e3d9f68-5b0b-a1c6-e813-6aaad624f7ac>
2011-06-06 05:05:08 +0000

    fix domAttrs_()

--- trunk/src/main/resources/web/js/soc/fb/mold/like-if.js
+++ trunk/src/main/resources/web/js/soc/fb/mold/like-if.js
@@ -4,6 +4,6 @@
 function (out) {
 	out.push('<iframe src="http://www.facebook.com/plugins/like.php?');
 	this._wgtAttrs(out, true);
-	out.push('" scrolling="no" frameborder="0" allowTransparency="true"', 
+	out.push('"', this.domAttrs_(), ' scrolling="no" frameborder="0" allowTransparency="true"', 
 			' style="border:none; overflow:hidden; width:450px; height:80px;"></iframe>'); // TODO: height & width
 }
--- trunk/src/main/resources/web/js/soc/fb/mold/like.js
+++ trunk/src/main/resources/web/js/soc/fb/mold/like.js
@@ -2,7 +2,7 @@
  * 
  */
 function (out) {
-	out.push('<fb:like');
+	out.push('<fb:like', this.domAttrs_());
 	this._wgtAttrs(out);
 	out.push('></fb:like>');
 }
